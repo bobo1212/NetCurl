1f315ba8c6bd73c1724dc84753480bff458e0c5b
bobo1212 <bobo1212@wp.pl>
2017-04-15 07:44:20 +0200

    Klasa do pobierani plików z sieći

---
+++ Net/Fget.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Net;
+
+/**
+ */
+class Fget {
+
+    public function getPage($url) {
+
+        $data = file_get_contents($url);
+
+        if (is_array($http_response_header)) {
+            foreach ($http_response_header as $c => $h) {
+                if (stristr($h, 'content-encoding') and stristr($h, 'gzip')) {
+                    $data = $this->gzdecode($data);
+                }
+            }
+        }
+        return $data;
+    }
+
+    private function gzdecode($data) {
+        return gzinflate(substr($data, 10, -8));
+    }
+
+}
